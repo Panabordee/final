9441fa1777081e7c45c930d5962996f758907474
Panabordee <68010697@kmitl.ac.th>
2025-10-07 23:05:50 +0700

    Add files via upload

---
+++ e2etests.h
@@ -0,0 +1,8 @@
+#ifndef E2ETESTS_H
+#define E2ETESTS_H
+
+#include "course.h" // Include main header for access to definitions
+
+void run_e2e_tests();
+
+#endif // E2ETESTS_H
---
+++ unittests.h
@@ -0,0 +1,8 @@
+#ifndef UNITTESTS_H
+#define UNITTESTS_H
+
+#include "course.h" // Include main header for access to definitions
+
+void run_unit_tests();
+
+#endif // UNITTESTS_H
