400ecde069640dd73d9f2075341bfee2620a69f6
Panabordee <68010697@kmitl.ac.th>
2025-10-07 22:58:32 +0700

    Update e2etests.h

--- e2etests.h
+++ e2etests.h
@@ -1,8 +1,9 @@
 #ifndef E2ETESTS_H
 #define E2ETESTS_H
 
-#include "course.h" // Include main header for access to definitions
+#include "course.h"
 
 void run_e2e_tests();
 
-#endif // E2ETESTS_H
+
+#endif 
